fdf9d9458204a02d7d6747d2b0bb339592318b6c
Vv-kK <veronicakylie1@gmail.com>
2023-11-15 14:43:21 +0700

    Mengerjakan Tugas 8

--- shopping_list/urls.py
+++ shopping_list/urls.py
@@ -21,9 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+    path('auth/', include('authentication.urls')),
 ]
-# urlpatterns = [
-#     ...
-#     path('main/', include('main.urls')),
-#     ...
-# ]
